e2d21dfd2f0a1c84d75469f6b2a401d4a58a55da
sulabhk <sulabh.kothari@gmail.com>
2019-02-16 12:35:30 +0530

    Type classes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
+package typeclass
+
 object Main{
   def main(args: Array[String]): Unit = {
-    println("H")
+
   }
 }
---
+++ src/main/scala/TheTypeClass.scala
@@ -0,0 +1,68 @@
+package typeclass
+
+// Define a very simple JSON AST
+sealed trait Json
+
+final case class JsObject(get: Map[String, Json]) extends Json
+
+final case class JsString(get: String) extends Json
+
+final case class JsNumber(get: Double) extends Json
+
+case object JsNull extends Json
+
+// The "serialize to JSON" behaviour is encoded in this trait
+trait JsonWriter[A] {
+  def write(value: A): Json
+}
+
+final case class Person(name: String, email: String)
+
+object JsonWriterInstances {
+  implicit val stringWriter: JsonWriter[String] =
+    new JsonWriter[String] {
+      def write(value: String): Json =
+        JsString(value)
+    }
+  implicit val personWriter: JsonWriter[Person] =
+    new JsonWriter[Person] {
+      def write(value: Person): Json =
+        JsObject(Map(
+          "name" -> JsString(value.name),
+          "email" -> JsString(value.email)
+        ))
+    }
+}
+
+object JsonSyntax {
+
+  implicit class JsonWriterOps[A](value: A) {
+    def toJson(implicit w: JsonWriter[A]): Json =
+      w.write(value)
+  }
+
+  implicit def optionWriter[A]
+  (implicit writer: JsonWriter[A]): JsonWriter[Option[A]] =
+    new JsonWriter[Option[A]] {
+      def write(option: Option[A]): Json =
+        option match {
+          case Some(aValue) => writer.write(aValue)
+          case None => JsNull
+        }
+    }
+
+}
+
+object Json {
+  def toJson[A](value: A)(implicit w: JsonWriter[A]): Json =
+    w.write(value)
+
+  def main(args: Array[String]): Unit = {
+    import JsonWriterInstances._
+    import JsonSyntax._
+    println( Person("Dave", "dave@example.com").toJson)
+    println( Option(Person("Dave", "dave@example.com")).toJson)
+    val person:Option[Person] = None
+    println( Option[Person](null).toJson)
+  }
+}
