b8cbf3e7c29ae187353ec07c3f99536add7487cd
sulabhk <sulabh.kothari@gmail.com>
2019-03-14 00:11:19 +0530

    imap: Codec example

--- src/main/scala/VariantFunctors.scala
+++ src/main/scala/VariantFunctors.scala
@@ -18,6 +18,7 @@ object ContravariantFunctor {
         def format(value: B): String =
           self.format(func(value))
       }
+
     //The contramap method only makes sense for data types that represent transformati􏰂ons
     //For example, we can’t define contramap for an Option because there is no way of
     // feeding a value in an Option[B] backwards through a function A => B
@@ -55,27 +56,47 @@ object InvariantFunctor {
   // an operation to a chain, imap generates them via a pair of bidirectional transformations.
 
   def main(args: Array[String]): Unit = {
-
+    println(encode(123.4))
+    println(decode[Double]("123.4"))
+    println(encode(Box(123.4)))
+    println(decode[Box[Double]]("123.4"))
   }
 
   trait Codec[A] {
+    self =>
     def encode(value: A): String
+
     def decode(value: String): A
-    def imap[B](dec: A => B, enc: B => A): Codec[B] = ???
+
+    def imap[B](dec: A => B, enc: B => A): Codec[B] = new Codec[B] {
+      override def encode(value: B): String = self.encode(enc(value))
+
+      override def decode(value: String): B = dec(self.decode(value))
+    }
   }
+
   def encode[A](value: A)(implicit c: Codec[A]): String = c.encode(value)
+
   def decode[A](value: String)(implicit c: Codec[A]): A = c.decode(value)
 
   implicit val stringCodec: Codec[String] =
     new Codec[String] {
       def encode(value: String): String = value
+
       def decode(value: String): String = value
     }
 
   implicit val intCodec: Codec[Int] =
     stringCodec.imap(_.toInt, _.toString)
+
   implicit val booleanCodec: Codec[Boolean] =
     stringCodec.imap(_.toBoolean, _.toString)
 
+  implicit val doubleCodec: Codec[Double] =
+    stringCodec.imap(_.toDouble, _.toString)
+
   case class Box[A](value: A)
+
+  implicit def boxCodec[A](implicit c: Codec[A]): Codec[Box[A]] = c.imap(Box(_), _.value)
+
 }
